0178e8a784a149d550eb436ffc1211dbca4649c9
azad-YM <02ym.azad@gmail.com>
2025-11-04 19:07:37 +0300

    Task: add triangle

--- struct_method_interface/shapes_test.go
+++ struct_method_interface/shapes_test.go
@@ -11,21 +11,26 @@ func Test_perimeter(t *testing.T) {
 }
 
 type TableTests struct {
-	shape Shape
-	want  float64
+	name    string
+	shape   Shape
+	hasArea float64
 }
 
 func TestArea(t *testing.T) {
-	testsArea := []TableTests{
-		{shape: Rectangle{12, 6}, want: 72},
-		{shape: Circle{10}, want: 314.1592653589793},
+	areaTests := []TableTests{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
-	for _, tt := range testsArea {
-		got := tt.shape.Area()
-		want := tt.want
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 
-		assertCorrectMessage(t, got, want)
 	}
 }
 
