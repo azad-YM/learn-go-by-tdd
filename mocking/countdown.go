0b63844bfda664e72c3cc13df3cec7a515683eaf
azad-YM <02ym.azad@gmail.com>
2025-11-07 12:03:53 +0300

    Task: mocking - print and sleep

---
+++ mocking/countdown.go
@@ -0,0 +1,38 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+)
+
+const write = "write"
+const sleep = "sleep"
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return len(s.Calls), nil
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, finalWord)
+}
