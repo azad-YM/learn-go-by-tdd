746450307aac5328b2bf923e27f04b54b4bacebd
azad-YM <02ym.azad@gmail.com>
2026-03-09 16:26:39 +0300

    ConvertToRoman - Refactor tests

--- roman_numeral/roman_numeral_test.go
+++ roman_numeral/roman_numeral_test.go
@@ -5,30 +5,23 @@ import (
 )
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run("1 gots roman I", func(t *testing.T) {
-		want := "I"
-		got := ConvertToRoman(1)
+	cases := []struct {
+		Description string
+		Number      int
+		Want        string
+	}{
+		{"1 gots roman I", 1, "I"},
+		{"2 gots roman II", 2, "II"},
+		{"3 gots roman III", 3, "III"},
+	}
 
-		if want != got {
-			t.Errorf("%q got, %q want", got, want)
-		}
-	})
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Number)
 
-	t.Run("2 gots roman II", func(t *testing.T) {
-		want := "II"
-		got := ConvertToRoman(2)
-
-		if want != got {
-			t.Errorf("%q got, %q want", got, want)
-		}
-	})
-
-	t.Run("3 gots roman III", func(t *testing.T) {
-		want := "III"
-		got := ConvertToRoman(3)
-
-		if want != got {
-			t.Errorf("%q got, %q want", got, want)
-		}
-	})
+			if test.Want != got {
+				t.Errorf("%q got, %q want", got, test.Want)
+			}
+		})
+	}
 }
