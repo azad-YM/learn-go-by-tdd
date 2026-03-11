51d602573dbb91a62d777674787127182294b116
azad-YM <02ym.azad@gmail.com>
2026-03-11 23:01:06 +0300

    Clockface - Refactor Display second hand

--- clockface/clockface.go
+++ clockface/clockface.go
@@ -1,8 +1,16 @@
 package clockface
 
 import (
+	"fmt"
+	"io"
 	"math"
 	"time"
+)
+
+const (
+	secondHandLength = 90
+	clockCentreX     = 150
+	clockCentreY     = 150
 )
 
 type Point struct {
@@ -10,13 +18,20 @@ type Point struct {
 	Y float64
 }
 
-func SecondHand(t time.Time) Point {
-	p := secondHandPoint(t)
-	p = Point{X: p.X * 90, Y: p.Y * 90}
-	p = Point{X: p.X, Y: -p.Y}
-	p = Point{X: p.X + 150, Y: p.Y + 150}
+func SVGWriter(w io.Writer, t time.Time) {
+	io.WriteString(w, svgStart)
+	io.WriteString(w, bezel)
+	secondHand(w, t)
+	io.WriteString(w, svgEnd)
+}
 
-	return p
+func secondHand(w io.Writer, t time.Time) {
+	p := secondHandPoint(t)
+	p = Point{X: p.X * secondHandLength, Y: p.Y * secondHandLength}
+	p = Point{X: p.X, Y: -p.Y}
+	p = Point{X: p.X + clockCentreX, Y: p.Y + clockCentreY}
+
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%f" y2="%f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
 }
 
 func secondHandPoint(t time.Time) Point {
@@ -29,3 +44,15 @@ func secondHandPoint(t time.Time) Point {
 func secondsInRadians(t time.Time) float64 {
 	return math.Pi / (30 / float64(t.Second()))
 }
+
+const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="100%"
+     height="100%"
+     viewBox="0 0 300 300"
+     version="2.0">`
+
+const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+
+const svgEnd = `</svg>`
--- clockface/clockface_acceptace_test.go
+++ clockface/clockface_acceptace_test.go
@@ -1,28 +1,84 @@
 package clockface_test
 
 import (
+	"bytes"
+	"encoding/xml"
+	"slices"
 	"testing"
 	"time"
 
 	"example.com/learn/clockface"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	time := time.Date(2019, time.October, 22, 0, 0, 0, 0, time.UTC)
-	want := clockface.Point{150, 150 - 90}
-	got := clockface.SecondHand(time)
+func TestSVGWriterSecondHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 60},
+		},
+		{
+			simpleTime(0, 0, 30),
+			Line{150, 150, 150, 150 + 90},
+		},
+		{
+			simpleTime(0, 0, 45),
+			Line{150, 150, 60, 150},
+		},
+		{
+			simpleTime(0, 0, 15),
+			Line{150, 150, 240, 150},
+		},
+	}
 
-	if want != got {
-		t.Errorf("Got %v, Want %v", got, want)
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			var b bytes.Buffer
+			clockface.SVGWriter(&b, c.time)
+			svg := Svg{}
+			xml.Unmarshal(b.Bytes(), &svg)
+
+			if !containsLine(c.line, svg.Line) {
+				t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			}
+		})
 	}
 }
 
-func TestSecondHandAt30Seconds(t *testing.T) {
-	time := time.Date(2019, time.October, 22, 0, 0, 30, 0, time.UTC)
-	want := clockface.Point{150, 150 + 90}
-	got := clockface.SecondHand(time)
+func containsLine(l Line, ls []Line) bool {
+	return slices.Contains(ls, l)
+}
 
-	if want != got {
-		t.Errorf("Got %v, Want %v", got, want)
-	}
+type Svg struct {
+	XMLName xml.Name `xml:"svg"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  Circle   `xml:"circle"`
+	Line    []Line   `xml:"line"`
+}
+
+type Circle struct {
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
+}
+
+type Line struct {
+	X1 float64 `xml:"x1,attr"`
+	Y1 float64 `xml:"y1,attr"`
+	X2 float64 `xml:"x2,attr"`
+	Y2 float64 `xml:"y2,attr"`
+}
+
+func simpleTime(h, m, s int) time.Time {
+	return time.Date(2019, time.October, 22, h, m, s, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
 }
